6d487c3e797353712e4f10be50b25b0855bddb4b
Stewart Hardwick <hardwick.stewart@gmail.com>
2019-05-12 21:19:13 +0100

    escape behaviour

--- smart_car_robot.py
+++ smart_car_robot.py
@@ -2,15 +2,16 @@ import time
 
 from arbitrator import Arbitrator
 from cruise import Cruise
+from escape import Escape
 from proximity_sensor import ProximitySensor
 from robot_base import RobotBase
 from smart_car_motor_controller import SmartCarMotorController
 
 motor_controller = SmartCarMotorController()
 
-front_left_ir_sensor = ProximitySensor(name='front_left_ir', pin=5)
-front_right_ir_sensor = ProximitySensor(name='front_right_ir', pin=6)
-front_ir_sensor = ProximitySensor(name='front_ir', pin=7)
+front_left_ir_sensor = ProximitySensor(name='front_left_ir', pin=20)
+front_right_ir_sensor = ProximitySensor(name='front_right_ir', pin=21)
+front_ir_sensor = ProximitySensor(name='front_ir', pin=26)
 
 sensors = [front_ir_sensor, front_left_ir_sensor, front_right_ir_sensor]
 
@@ -20,8 +21,9 @@ max_speed = 0.2
 base = RobotBase(wheel_separation, max_speed, motor_controller, sensors)
 
 cruise_behaviour = Cruise(0)
+escape_behaviour = Escape(1)
 
-behaviours = [cruise_behaviour]
+behaviours = [cruise_behaviour, escape_behaviour]
 
 arbitrator = Arbitrator(behaviours)
 
