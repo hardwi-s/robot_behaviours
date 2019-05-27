7a4d1889d5a8ada4c3a94a1d73b43998c6662690
Stewart Hardwick <hardwick.stewart@gmail.com>
2019-05-27 13:41:55 +0100

    motor speed factors

--- smart_car_motor_controller.py
+++ smart_car_motor_controller.py
@@ -2,6 +2,10 @@ from dual_motor_controller import DualMotorController
 from gpiozero import Motor
 
 MAX_SPEED_M_PER_SEC = 0.52
+RIGHT_FWD_FACTOR = 0.8
+RIGHT_REV_FACTOR = 1.0
+LEFT_FWD_FACTOR = 0.8
+LEFT_REV_FACTOR = 1.0
 
 
 class SmartCarMotorController(DualMotorController):
@@ -16,11 +20,11 @@ class SmartCarMotorController(DualMotorController):
         speed_right = speed_right_m_per_sec / MAX_SPEED_M_PER_SEC
 
         if speed_left < 0:
-            self._left_motor.backward(-speed_left)
+            self._left_motor.backward(-speed_left * LEFT_REV_FACTOR)
         else:
-            self._left_motor.forward(speed_left)
+            self._left_motor.forward(speed_left * LEFT_FWD_FACTOR)
 
         if speed_right < 0:
-            self._right_motor.backward(-speed_right)
+            self._right_motor.backward(-speed_right * RIGHT_REV_FACTOR)
         else:
-            self._right_motor.forward(speed_right)
+            self._right_motor.forward(speed_right * RIGHT_FWD_FACTOR)
