9879d0a4c65223f31f8c310ec60f462cbcbe84ea
Stewart Hardwick <hardwick.stewart@gmail.com>
2019-06-24 21:03:58 +0100

    Romi motor speed conversion

--- romi_robot_base.py
+++ romi_robot_base.py
@@ -1,7 +1,12 @@
+from math import pi
+
 from robot_base import RobotBase
 
 TICKS_PER_REV = 1440.0
 WHEEL_DIAMETER = 0.07
+WHEEL_CIRCUMFERENCE = pi * WHEEL_DIAMETER
+ROMI_ENCODER_INTERVAL = 50.0 / 1000.0   # 50 msec
+TICKS_PER_M = TICKS_PER_REV / WHEEL_CIRCUMFERENCE
 
 
 class RomiRobotBase(RobotBase):
@@ -29,5 +34,11 @@ class RomiRobotBase(RobotBase):
         # Speeds are in m/sec, convert to Romi speeds
         self._a_star.motors(self._romi_speed(speed_left), self._romi_speed(speed_right))
 
-    def _romi_speed(self, motor_speed):
-        return motor_speed
+    def _romi_speed(self, motor_speed_m_per_sec):
+        '''
+        1 m/sec = (1440 / (pi * 0.07)) * 0.05 ticks/cycle
+        speed = m/sec * 327.42
+        :param motor_speed_m_per_sec:
+        :return: motor speed in ticks per loop
+        '''
+        return (motor_speed_m_per_sec * TICKS_PER_M) * ROMI_ENCODER_INTERVAL
