a2fe4ee962b262004bed708c0a6855c7184ed6da
Stewart Hardwick <stewart.hardwick@redembedded.com>
2019-07-30 12:50:32 +0100

    More work on pose estimation

--- romi/romi_pose_sensor.py
+++ romi/romi_pose_sensor.py
@@ -2,16 +2,20 @@ from threading import Lock
 
 
 class RomiPoseSensor:
-    def __init__(self, name='pose', pose=None, base=None):
+    def __init__(self, name='pose', pose=None, base=None, encoders=None):
         self._name = name
         if pose is None:
             pose = {'x': 0.0, 'y': 0.0, 'theta': 0.0}
         self._pose = pose
         self._base = base
+        self._encoders = encoders
         self._lock = Lock()
 
     def _update(self):
         current_motion_command = self._base.get_motion_command()
+        new_encoders = self._encoders.value
+        distance_left = new_encoders[0]
+        distance_right = new_encoders[1]
         with self._lock:
             if current_motion_command.rotation == 0:
                 pass
